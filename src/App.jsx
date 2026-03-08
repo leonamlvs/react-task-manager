70961aee7c28bca3eb909e6840a1908ba1fb85ce
Leonam Alves <leonamlvs@gmail.com>
2026-03-08 00:08:26 -0300

    feat: add tasks component with static list and completion toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Tasks from './components/Tasks';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Estudar programação',
+      description: 'Estudar programação para me tornar um desenvolvedor full stack.',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: 'Fazer exercícios físicos',
+      description: 'Fazer exercícios físicos para manter a saúde e o bem-estar.',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: 'Estudar matemática',
+      description: 'Estudar matemática para aprimorar as habilidades lógicas.',
+      isCompleted: false
+    }
+  ])
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+
+      // Update the task with the given id, toggling its isCompleted property
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // For all other tasks, return them unchanged
+      return task;
+    });
+
+    setTasks(newTasks);
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
