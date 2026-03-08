f3192060e4d63539e769e9996011474e2320fbb1
Leonam Alves <leonamlvs@gmail.com>
2026-03-07 22:30:51 -0300

    chore: update eslint config to turn off react prop types

--- eslint.config.js
+++ eslint.config.js
@@ -35,6 +35,7 @@ export default [
         "warn",
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off',
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
